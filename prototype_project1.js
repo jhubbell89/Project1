7ce8704eda3a244557342f7526fb4dfaabcfd2bd
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-10-20 07:37:26 -0700

    work on grid function

--- prototype_project1.js
+++ prototype_project1.js
@@ -1,5 +1,5 @@
 //variables needed to run game before it starts
-const board = document.querySelectorAll ('.board')
+const board = document.querySelectorAll ('.board div')
 const statusText = document.querySelector ("#messagedisplay")
 const restartBtn = document.querySelector ("#resetbtn")
 const redBtn = document.querySelector ("#redplayerbtn")
@@ -63,12 +63,11 @@ const winConditions = [
 
 //program needs to loop checking for win, tie conditions untill one is met and display message
 function checkWin() {
-    for (let z = 0; z < winConditions.length; z++) {
-        const spot1 = board[winConditions[z][0]]
-        const spot2 = board[winConditions[z][1]]
-        const spot3 = board[winConditions[z][2]]
-        const spot4 = board[winConditions[z][3]]
-
+    for (let y = 0; y < winConditions.length; y++) {
+        const spot1 = board[winConditions[y][0]]
+        const spot2 = board[winConditions[y][1]]
+        const spot3 = board[winConditions[y][2]]
+        const spot4 = board[winConditions[y][3]]
         if (
             spot1.classList.contains('red') &&
             spot2.classList.contains('red') &&
@@ -76,7 +75,6 @@ function checkWin() {
             spot4.classList.contains('red')
         )
         statusText.textContent = `Red Wins!!!`
-
         if (
             spot1.classList.contains('blue') &&
             spot2.classList.contains('blue') &&
@@ -106,19 +104,17 @@ function setBlue(event) {
 //function to set up cursor on mouse click change cell color
 for (let z = 0; z < board.length; z++) {
     board[z].onclick = () => {
-            if (board[z+6].classList.contains('full') &&!board[z].classList.contains('full')) {
+            if (board[z + 6].classList.contains('full') &&!board[z].classList.contains('full')) {
                 if (currentPlayer == 'red') {
                     board[z].classList.add('full')
-                    board[z].classlist.add('red')
+                    board[z].classList.add('red')
                     currentPlayer = 'blue'
                     statusText.textContent = `${currentPlayer}'s turn`
-                    document.getElementById('z').style.backgroundColor = 'red'
                 } else if (currentPlayer == 'blue') {
                     board[z].classList.add('full')
-                    board[z].classList.add('full')
+                    board[z].classList.add('blue')
                     currentPlayer = 'red'
                     statusText.textContent = `${currentPlayer}'s turn`
-                    document.getElementById('z').style.backgroundColor = 'blue'
                 }
             } else {statusText.textContent = 'Cant go here!'}
             checkWin()
